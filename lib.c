598926a0e78baca7b83ed35f392af03378fbcc75
Mihail Konev <k.mvc@ya.ru>
2017-06-04 15:59:31 +0500

    also perform WX -> W|X scan

--- lib.c
+++ lib.c
@@ -7,6 +7,8 @@
 #define PR_LOCKDOWN_MPROT 48
 #define PR_LOCKDOWN_MPROT_X 1
 #define PR_LOCKDOWN_MPROT_WX 2
+#define PR_LOCKDOWN_MPROT_STRIP_WX_X 3
+#define PR_LOCKDOWN_MPROT_STRIP_WX_W 4
 
 __attribute__((constructor))
 void activate_MProtect(void) {
@@ -15,6 +17,15 @@ void activate_MProtect(void) {
     prot = PR_LOCKDOWN_MPROT_WX;
 #if DISABLE_EXEC_PAGES
     prot = PR_LOCKDOWN_MPROT_X;
+#endif
+
+    prctl(PR_LOCKDOWN_MPROT, prot);
+
+    /* Stripping should be performed after the lockdown.  */
+
+    prot = PR_LOCKDOWN_MPROT_STRIP_WX_W;
+#if DISABLE_EXEC_PAGES
+    prot = PR_LOCKDOWN_MPROT_STRIP_WX_X;
 #endif
 
     prctl(PR_LOCKDOWN_MPROT, prot);
